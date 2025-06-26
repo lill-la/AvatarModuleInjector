58f6a923618659bcb5ba764f9e2fbc0ac7053649
NepuShiro <nepushiro@nepunep.xyz>
2025-06-26 02:10:07 -0500

    Bump Version

--- AvatarModuleInjector/AvatarModuleInjector.cs
+++ AvatarModuleInjector/AvatarModuleInjector.cs
@@ -16,7 +16,7 @@ namespace AvatarModuleInjector;
 
 public class AvatarModuleInjector : ResoniteMod
 {
-    internal const string VERSION_CONSTANT = "0.5.0";
+    internal const string VERSION_CONSTANT = "0.8.0";
     public override string Name => "AvatarModuleInjector";
     public override string Author => "lill";
     public override string Version => VERSION_CONSTANT;
