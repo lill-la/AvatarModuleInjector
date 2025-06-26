0364815927a1f0e44cf9027fa26d12420aa7c231
lill <lill@lill.la>
2025-06-26 14:51:31 +0900

    v0.7.0

--- AvatarModuleInjector/AvatarModuleInjector.cs
+++ AvatarModuleInjector/AvatarModuleInjector.cs
@@ -14,7 +14,7 @@ using ResoniteModLoader;
 namespace AvatarModuleInjector;
 
 public class AvatarModuleInjector : ResoniteMod {
-	internal const string VERSION_CONSTANT = "0.6.0";
+	internal const string VERSION_CONSTANT = "0.7.0";
 	public override string Name => "AvatarModuleInjector";
 	public override string Author => "lill";
 	public override string Version => VERSION_CONSTANT;
